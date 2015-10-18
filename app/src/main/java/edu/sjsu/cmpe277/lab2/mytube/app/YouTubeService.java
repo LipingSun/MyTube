7ba26f2b97bf30adbe1bcf8bc11f4f9a6b424e71
LipingSun <liping.sun@sjsu.edu>
2015-10-18 09:36:24 -0700

    implement search & getOrCreateFavorList

--- app/src/main/java/edu/sjsu/cmpe277/lab2/mytube/app/YouTubeService.java
+++ app/src/main/java/edu/sjsu/cmpe277/lab2/mytube/app/YouTubeService.java
@@ -1,6 +1,8 @@
 package edu.sjsu.cmpe277.lab2.mytube.app;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpRequest;
@@ -8,8 +10,7 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.SearchListResponse;
-import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,29 +21,29 @@ public class YouTubeService {
 
     private YouTube youtube;
 
-
     public YouTubeService(Context context, String token) {
         GoogleCredential credential = new GoogleCredential().setAccessToken(token);
         youtube = new YouTube.Builder(AndroidHttp.newCompatibleTransport(), JacksonFactory.getDefaultInstance(), credential)
                 .setApplicationName(context.getString(R.string.app_name)).build();
     }
 
-    List<VideoItem> search(String keyword) {
+    public List<VideoItem> search(String keyword) {
         try {
             //TODO: liping, please add required fields for video list
-            YouTube.Search.List search = youtube.search().list("id,snippet");
-            search.setType("video");
-            search.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
-            search.setQ(keyword);
+            YouTube.Search.List searchCommand = youtube.search().list("id,snippet");
+            searchCommand.setType("video");
+            searchCommand.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
+            searchCommand.setQ(keyword);
 
-            SearchListResponse response = search.execute();
+            SearchListResponse response = searchCommand.execute();
             List<SearchResult> results = response.getItems();
             List<VideoItem> items = new ArrayList<VideoItem>();
 
             for (SearchResult result : results) {
                 VideoItem item = new VideoItem();
                 item.setTitle(result.getSnippet().getTitle());
-                item.setDescription(result.getSnippet().getDescription());
+//                item.setNumOfViews(result.getSnippet().get);
+                item.setPublishDate(result.getSnippet().getPublishedAt().toString());
                 item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
                 item.setId(result.getId().getVideoId());
                 items.add(item);
@@ -55,8 +56,79 @@ public class YouTubeService {
         return null;
     }
 
+    public String playlistInsert(String title) {
+        PlaylistSnippet playlistSnippet = new PlaylistSnippet();
+        playlistSnippet.setTitle(title);
+        playlistSnippet.setDescription("A private playlist created with the YouTube API v3");
+        PlaylistStatus playlistStatus = new PlaylistStatus();
+        playlistStatus.setPrivacyStatus("private");
+        Playlist youTubePlaylist = new Playlist();
+        youTubePlaylist.setSnippet(playlistSnippet);
+        youTubePlaylist.setStatus(playlistStatus);
+
+        try {
+            YouTube.Playlists.Insert playlistInsertCommand =
+                    youtube.playlists().insert("snippet,status", youTubePlaylist);
+
+            Playlist playlistInserted = playlistInsertCommand.execute();
+            return playlistInserted.getId();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String listOrInsertFavorlist() {
+        try {
+            YouTube.Playlists.List playlistListCommand = youtube.playlists().list("snippet,contentDetails");
+            playlistListCommand.setMine(true);
+            PlaylistListResponse playlistListResponse = playlistListCommand.execute();
+            List<Playlist> playlists = playlistListResponse.getItems();
+            Playlist favorPlaylist = null;
+            for (Playlist playlist : playlists) {
+                if (playlist.getSnippet().getTitle() == "SJSU-CMPE-277") {
+                    favorPlaylist = playlist;
+                    return favorPlaylist.getId();
+                }
+            }
+            if (favorPlaylist == null) {
+                return playlistInsert("SJSU-CMPE-277");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    List<VideoItem> getPlaylistVideos(String playlistId) {
+        try {
+            YouTube.PlaylistItems.List playlistItemCommand =
+                    youtube.playlistItems().list("snippet");
+            playlistItemCommand.setPlaylistId(playlistId);
+            PlaylistItemListResponse playlistItemListResponse = playlistItemCommand.execute();
+
+            List<PlaylistItem> results = playlistItemListResponse.getItems();
+            List<VideoItem> items = new ArrayList<VideoItem>();
+
+            for (PlaylistItem result : results) {
+                VideoItem item = new VideoItem();
+                item.setTitle(result.getSnippet().getTitle());
+                item.setPublishDate(result.getSnippet().getPublishedAt().toString());
+                item.setThumbnailURL(result.getSnippet().getThumbnails().getDefault().getUrl());
+                item.setId(result.getSnippet().getResourceId().getVideoId());
+                items.add(item);
+            }
+
+            return items;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 //    void play(VideoItem videoItem);
-//    void createFavoriteList();
-//    List<VideoItem> getFavoriteList();
 //    void addFavorite(VideoItem videoItem);
 }
